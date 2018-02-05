1617fa88e2cdce1abb080cf1fbe1e14361671e3e
moinulIslamRobin <moinul.i.robin@gmail.com>
2018-02-05 12:26:54 +0600

    Load with respect to hall along with date and Time

--- Software/R&D/beta0.1/beta0.1.ino
+++ Software/R&D/beta0.1/beta0.1.ino
@@ -2,6 +2,7 @@
 #include <ESP8266WiFi.h>
 #include <ArduinoJson.h>
 #include <HX711.h>
+#include <time.h>
 //==============================================================================================
 // Scale Settings
 const int SCALE_DOUT_PIN = D2;
@@ -14,19 +15,21 @@ int hallState = 0;          // variable for reading the hall sensor status
 float value = 0;
 HX711 scale(SCALE_DOUT_PIN, SCALE_SCK_PIN);
 
+int timezone = 6 * 3600;
+int dst = 0;
 //==============================================================================================
  
 void setup() {
  
   Serial.begin(115200);                            //Serial connection
-  //============================================================================================
+//============================================================================================
   scale.set_scale(2230);// <- set here calibration factor!!!
   scale.tare();
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);      
   // initialize the hall effect sensor pin as an input:
   pinMode(hallPin, INPUT);
-  //============================================================================================
+//============================================================================================
   WiFi.begin("DataSoft_WiFi", "support123");   //WiFi connection
  
   while (WiFi.status() != WL_CONNECTED) {  //Wait for the WiFI connection completion
@@ -36,27 +39,10 @@ void setup() {
  
   }
   Serial.println("WiFi Connected!");
- 
+//=============================================================================================
+  configTime(timezone, dst, "pool.ntp.org","time.nist.gov");
 }
 //==============================================================================================
-//float HallEffect(){
-//  hallState = digitalRead(hallPin);   // read the state of the hall effect sensor:
-//  // Magnet near == LOW
-//  if (hallState == LOW) {     
-//    // turn LED on:  
-//    Serial.println("!!! LID ON !!!");  
-//    digitalWrite(ledPin, LOW);  
-//  } 
-//  else if (hallState == HIGH) {
-//    // turn LED off:
-//    digitalWrite(ledPin, HIGH);
-//    Serial.println("!!! LID OFF !!!");
-//    value = LoadCell(); // Call LoadCell function
-//    Serial.print("In HallEffect ");
-//    Serial.println(value);
-//  }
-//}
-
 float LoadCell(){
     float weight = scale.get_units(1); //loadcell
     //weight = weight + 90;
@@ -76,15 +62,27 @@ void loop() {
   if (hallState == LOW){
     Serial.println("!!! LID ON, Time to take the reading and send it to server!!!");  
     digitalWrite(ledPin, HIGH);
-    value = LoadCell(); 
+    value = LoadCell();
+    time_t now = time(nullptr);
+    struct tm* p_tm = localtime(&now);
+    String day = String(p_tm->tm_mday);
+    String mon = String(p_tm->tm_mon+1);
+    String year = String(p_tm->tm_year + 1900);
+    String hour = String(p_tm->tm_hour);
+    String mint = String(p_tm->tm_min);
+    String sec = String(p_tm->tm_sec);
+    String curTime = hour+":"+mint+":"+sec;
+    String daymon = day+"/"+mon+"/"+year+"  "+curTime;
+    Serial.print(daymon); 
     if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status      
       StaticJsonBuffer<300> JSONbuffer;   //Declaring static JSON buffer
       JsonObject& JSONencoder = JSONbuffer.createObject(); 
    
-      JSONencoder["sensorType"] = "LoadCell";
+      JSONencoder["sensorType"] = "LoadCell - Date and Time";
    
-      JsonArray& values = JSONencoder.createNestedArray("Gram"); //JSON array
+      JsonArray& values = JSONencoder.createNestedArray("Gram - Date and Time"); //JSON array
       values.add(value); //Add value to array
+      values.add(daymon); //Add Date and Time to array
       //==========================================================================================
       char JSONmessageBuffer[300];
       JSONencoder.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
@@ -92,7 +90,7 @@ void loop() {
    
       HTTPClient http;    //Declare object of class HTTPClient
    
-      http.begin("http://192.168.6.77:8081/value");      //Specify request destination
+      http.begin("http://192.168.6.176:8081/value");      //Specify request destination
       http.addHeader("Content-Type", "application/json");  //Specify content-type header
    
       int httpCode = http.POST(JSONmessageBuffer);   //Send the request
@@ -109,7 +107,7 @@ void loop() {
    
     }
  
-  delay(5000);  //Send a request every 30 seconds
+  delay(30000);  //Send a request every 30 seconds
   }
   else if (hallState == HIGH){
     Serial.println("!!! LID OFF, Time to take my timely MEDICINE!!!");  
