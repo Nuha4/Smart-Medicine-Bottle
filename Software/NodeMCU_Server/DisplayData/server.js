ae3d28fed7e86363f0485a7e56c23d378dcc9223
Nuha4 <sumaiya.tasmim.cse@ulab.edu.bd>
2018-02-07 12:02:09 +0600

    Missed Dose Check and Send Email

--- Software/NodeMCU_Server/DisplayData/server.js
+++ Software/NodeMCU_Server/DisplayData/server.js
@@ -16,41 +16,54 @@ var smtpTransport = nodemailer.createTransport({
     }
 });
 
-
 var MongoClient = require('mongodb').MongoClient;
 MongoClient.connect('mongodb://localhost:27017/MedicineDB', function (err, client) {
-  if (err) throw err;
-  var db = client.db('MedicineDB');
-  db.collection('data_table').findOne({}, function (findErr, result) {
-    if (findErr) throw findErr;
-    console.log(result);
-  });
+    if (err) throw err;
+    var db = client.db('MedicineDB');
+    /*db.collection('data_table').findOne({}, function (findErr, result) {
+      if (findErr) throw findErr;
+      console.log(result);
+    });*/
 
-  app.get('/', (req, res) => {
-     /*var cursor = db.collection('data_table').find();
-     console.log(cursor);*/
-    //db.collection('data_table').findOne({}, function (findErr, result) {
-    db.collection('data_table').find().toArray((err, result) => {
-        if (err) throw err;
-        console.log(result);
-        res.render('index.ejs', {CellData: result});
+    app.get('/', (req, res) => {
+       /*var cursor = db.collection('data_table').find();
+       console.log(cursor);*/
+      //db.collection('data_table').findOne({}, function (findErr, result) {
+      db.collection('data_table').find().toArray((err, result) => {
+          if (err) throw err;
+          //console.log(result);
+          res.render('index.ejs', {CellData: result});
+      });
+      //res.sendFile(__dirname + '/index.html');
     });
-    //res.sendFile(__dirname + '/index.html');
-  });
-});
 
-app.get('/sendmail', function(req, res) {
-    setInterval(function(){
-        sendEmail();
-    },5000);
+    app.get('/sendmail', function(req, res) {
+        db.collection('data_table').find().toArray((err, result) => {
+            if (err) throw err;
+            var ln = result.length;
+            for(var i=0; i<ln; i++){
+              console.log("value : " + result[i].value);
+              var LastWeight = result[ln-2].value;
+              var currentWeight = result[ln-1].value;
+            }
+            if( LastWeight > currentWeight){
+              console.log("Dose Taken");
+            }
+            else {
+              console.log("Dose Missed");
+              setInterval(function(){
+                  sendEmail();
+              },10000);
+            }
+            res.render('index.ejs', {CellData: result});
+        });
+    });
 });
-
 
 function sendEmail(){
   var mailOptions = {
       to:"nuha.khan4@gmail.com",
       subject:"Email from nodemailer",
-      //html:'<div>Name: '+ req.query.name +'</div><div>Email: '+ req.query.email +'</div><div>Mobile: '+ req.query.mobile +'</div><div>Message: '+ req.query.message +'</div>'
       html: 'LoadCell Data'
   }
   smtpTransport.sendMail(mailOptions, function(error, response) {
@@ -65,22 +78,3 @@ function sendEmail(){
 app.listen(3000, function() {
   console.log('listening on 3000');
 });
-
-/*app.post('/quotes', (req, res) => {
-  console.log(req.body) // Take data From Browser
-});*/
-//res.render(view, locals)
-/*app.post('/data', (req, res) => {
-  db.collection('data_table').save(req.body, (err, result) => {
-    if (err) return console.log(err)
-
-    console.log('saved to database')
-    res.redirect('/')
-  })
-})*/
-/*app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-  // Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get!
-  // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
-});
-*/
