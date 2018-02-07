c6e3121019e2a4652b4f00a5fc28a07655143b7c
Nuha4 <sumaiya.tasmim.cse@ulab.edu.bd>
2018-02-07 09:54:48 +0600

    Notification Send via enail

--- Software/NodeMCU_Server/DisplayData/server.js
+++ Software/NodeMCU_Server/DisplayData/server.js
@@ -1,42 +1,115 @@
 const express = require('express');
 const app = express();
 const bodyParser= require('body-parser');
+const nodemailer = require('nodemailer');
 
 app.set('view engine', 'ejs')
-
 app.use(bodyParser.urlencoded({extended: true}));
+
+
+var smtpTransport = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    auth: {
+        user: "moinul.f18g01@gmail.com",
+        pass: "floor18group1"
+    }
+});
+
 
 var MongoClient = require('mongodb').MongoClient;
 MongoClient.connect('mongodb://localhost:27017/MedicineDB', function (err, client) {
   if (err) throw err;
-
   var db = client.db('MedicineDB');
-
   db.collection('data_table').findOne({}, function (findErr, result) {
     if (findErr) throw findErr;
     console.log(result);
-    //client.close();
   });
 
   app.get('/', (req, res) => {
      /*var cursor = db.collection('data_table').find();
      console.log(cursor);*/
-
     //db.collection('data_table').findOne({}, function (findErr, result) {
     db.collection('data_table').find().toArray((err, result) => {
         if (err) throw err;
         console.log(result);
-        //client.close();
-        res.render('index.ejs', {quotes: result});
+        res.render('index.ejs', {CellData: result});
     });
     //res.sendFile(__dirname + '/index.html');
   });
 });
 
-
-app.post('/quotes', (req, res) => {
-  console.log(req.body) // Take data From Browser
+app.get('/sendmail', function(req, res) {
+    var mailOptions = {
+        to:"nuha.khan4@gmail.com",
+        subject:"Email from nodemailer",
+        html:'<div>Name: '+ req.query.name +'</div><div>Email: '+ req.query.email +'</div><div>Mobile: '+ req.query.mobile +'</div><div>Message: '+ req.query.message +'</div>'
+    }
+    smtpTransport.sendMail(mailOptions, function(error, response) {
+     if(error) {
+        res.end("error");
+     } else {
+        res.end("sent");
+     }
+   });
 });
+
+
+app.listen(3000, function() {
+  console.log('listening on 3000');
+});
+
+
+
+
+
+// Generate test SMTP service account from ethereal.email
+// Only needed if you don't have a real mail account for testing
+    /*nodemailer.createTestAccount((err, account) => {
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+            host: 'smtp.ethereal.email',
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+                user: account.user, // generated ethereal user
+                pass: account.pass  // generated ethereal password
+            }
+        });
+        // setup email data with unicode symbols
+        let mailOptions = {
+            from: '"Fred Foo 👻" <foo@example.com>', // sender address
+            to: 'bar@example.com, baz@example.com', // list of receivers
+            subject: 'Hello ✔', // Subject line
+            text: 'Hello world?', // plain text body
+            html: '<b>Hello world?</b>' // html body
+        };
+        // send mail with defined transport object
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message sent: %s', info.messageId);
+            // Preview only available when sending through an Ethereal account
+            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+            // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+        });
+    });*/
+
+
+
+
+
+
+
+
+
+
+/*app.post('/quotes', (req, res) => {
+  console.log(req.body) // Take data From Browser
+});*/
 
 
 //res.render(view, locals)
@@ -69,7 +142,3 @@ MongoClient.connect(url, (err, client) => {
   // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
 });
 */
-
-app.listen(3000, function() {
-  console.log('listening on 3000');
-});
